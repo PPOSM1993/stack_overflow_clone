ce01195f33c83fd9c573565e0c99f2c87a217726
Administrador <administrador@ubuntudocker.myguest.virtualbox.org>
2024-06-10 18:55:26 -0400

    avance stack overflow

---
+++ stackoverflow/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'stack_overflow'
+
+urlpatterns = [
+    path('', views.index),
+    path('home/', views.HomeView, name='home'),
+    path('about/', views.AboutView, name='about'),
+]
---
+++ stackoverflow/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def HomeView(request):
+    return render(request, 'home.html')
+
+def AboutView(request):
+    return render(request, 'about.html')
